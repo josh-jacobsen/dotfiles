8308731dcde20d00b2d2140f8c87e0417b72fe7e
Josh <jdjacobsen@protonmail.com>
2024-07-12 12:35:25 +1200

    trial lua copilot

--- nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
@@ -99,8 +99,9 @@ return {
         },
 
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
+          { name = 'copilot', group_index = 2 },
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'luasnip', group_index = 2 },
           { name = 'path' },
           { name = 'buffer' }, -- text within current buffer
         },
