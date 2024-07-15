63e3d8e1bf70a60a572c5145cebb76e5477b934b
Josh <jdjacobsen@protonmail.com>
2024-07-16 10:08:38 +1200

    Add rest of group index settings

--- nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
@@ -102,8 +102,8 @@ return {
           { name = 'copilot', group_index = 2 },
           { name = 'nvim_lsp', group_index = 2 },
           { name = 'luasnip', group_index = 2 },
-          { name = 'path' },
-          { name = 'buffer' }, -- text within current buffer
+          { name = 'path', group_index = 2 },
+          { name = 'buffer', group_index = 2 }, -- text within current buffer
         },
         formatting = {
           format = lspkind.cmp_format {
