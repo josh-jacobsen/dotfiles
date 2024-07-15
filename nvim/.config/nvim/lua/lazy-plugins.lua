80da964e9edc209000e712f503f667cf58da93e8
Josh <jdjacobsen@protonmail.com>
2024-07-16 09:57:59 +1200

    Add chatgpt plugin

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -70,6 +70,7 @@ require('lazy').setup({
   require 'carnifx/plugins/dap',
   require 'carnifx/plugins/oil',
   require 'carnifx/plugins/neogit',
+  require 'carnifx/plugins/chatgpt',
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
