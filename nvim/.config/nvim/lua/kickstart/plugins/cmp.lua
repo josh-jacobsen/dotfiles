158aab35251991fc5338b5f6e7b570b56e74c6e5
Josh Jacobsen <josh@multitudes.co>
2024-07-17 17:01:25 -0700

    Add copilot  (#2)

--- nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/cmp.lua
@@ -99,10 +99,11 @@ return {
         },
 
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'buffer' }, -- text within current buffer
+          { name = 'copilot', group_index = 2 },
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'luasnip', group_index = 2 },
+          { name = 'path', group_index = 2 },
+          { name = 'buffer', group_index = 2 }, -- text within current buffer
         },
         formatting = {
           format = lspkind.cmp_format {
--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -30,6 +30,13 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
+
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
@@ -68,6 +75,7 @@ require('lazy').setup({
   require 'carnifx/plugins/dap',
   require 'carnifx/plugins/oil',
   require 'carnifx/plugins/neogit',
+  require 'carnifx/plugins/copilot',
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
