0bbea0833218ce996b3507cbcf14b4268eec63cd
Josh <jdjacobsen@protonmail.com>
2024-07-20 11:24:02 +1200

    Update packages after updating neovim to 0.10

--- nvim/.config/nvim/lua/carnifx/plugins/neogit.lua
+++ nvim/.config/nvim/lua/carnifx/plugins/neogit.lua
@@ -1,12 +1,9 @@
 return {
   'NeogitOrg/neogit',
-  tag = 'v0.0.1',
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
-
     'nvim-telescope/telescope.nvim', -- optional
-    -- 'ibhagwan/fzf-lua', -- optional
   },
   -- config = true,
   config = function()
