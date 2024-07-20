025864490dcb7bcd17ed85c90b3a6ebd85768c77
Josh <jdjacobsen@protonmail.com>
2024-07-20 22:14:59 +1200

    Add description to Gitsigns keymaps

--- nvim/.config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -35,29 +35,28 @@ return {
               gitsigns.nav_hunk 'prev'
             end
           end)
-
           -- Actions
-          map('n', '<leader>hs', gitsigns.stage_hunk)
-          map('n', '<leader>hS', gitsigns.stage_buffer)
-          map('n', '<leader>hr', gitsigns.reset_hunk)
-          map('n', '<leader>hR', gitsigns.reset_buffer)
+          map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage Hunk' })
+          map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage Buffer' })
+          map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset Hunk' })
+          map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset Buffer' })
           map('v', '<leader>hs', function()
             gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
           end)
           map('v', '<leader>hr', function()
             gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
           end)
-          map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-          map('n', '<leader>hp', gitsigns.preview_hunk)
-          map('n', '<leader>hb', function()
+          map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Unstage Hunk' })
+          map('n', '<leader>hv', gitsigns.preview_hunk_inline, { desc = 'Hunk Preview' })
+          map('n', '<leader>hl', function()
             gitsigns.blame_line { full = true }
-          end)
-          map('n', '<leader>tb', gitsigns.toggle_current_line_blame) -- ghost text
-          map('n', '<leader>hd', gitsigns.diffthis)
+          end, { desc = 'Blame Line' })
+          map('n', '<leader>hb', gitsigns.toggle_current_line_blame, { desc = 'Toggle Blame' }) -- ghost text
+          map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff' })
           map('n', '<leader>hD', function()
             gitsigns.diffthis '~'
-          end)
-          map('n', '<leader>td', gitsigns.toggle_deleted)
+          end, { desc = 'Diff All' })
+          map('n', '<leader>he', gitsigns.toggle_deleted, { desc = 'Toggle Deleted Lines' })
 
           -- Text object
           map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
