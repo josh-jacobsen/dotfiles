b83c519aa146f184c647949ba529994b42cfa10f
Josh <jdjacobsen@protonmail.com>
2025-06-21 21:45:50 +1200

    add back diagnostic symbols

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -136,7 +136,7 @@ return {
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       -- Change the Diagnostic symbols in the sign column (gutter)
-      local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+      local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
       for type, icon in pairs(signs) do
         local hl = 'DiagnosticSign' .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
